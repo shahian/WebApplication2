0f3d073c622181a232fa2be032dfde9c41511fac
shahian <hamidreza.shahian@gmail.com>
2019-08-05 08:30:13 +0430

    0000

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -17,6 +17,7 @@ namespace WebApplication2.Controllers
 
         public ActionResult About()
         {
+
             ViewBag.Message = "Your application description page.";
 
             return View();
