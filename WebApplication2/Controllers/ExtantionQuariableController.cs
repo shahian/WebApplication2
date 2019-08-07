c432839447d7be5e3ae101a6afd921690b1d49fe
shahian <hamidreza.shahian@gmail.com>
2019-08-07 08:26:44 +0430

    a

---
+++ WebApplication2/Controllers/ExtantionQuariableController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.infrastructure;
+
+namespace WebApplication2.Controllers
+{
+    public class ExtantionQuariableController : Controller
+    {
+        // GET: ExtantionQuariable
+        public ActionResult Index()
+        {
+            var Users = new List<User>
+        {
+            new User { Name = "Mahesh Chand", Id = 5 },
+            new User { Name = "Neel Beniwal", Id = 95 },
+            new User { Name = "Chris Love", Id =15 }
+        }.AsQueryable();
+            var fff=Users.First(x => x.Name == "");
+            var jjj=Users.ByUserName("Mahesh Chand");
+            return View();
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ WebApplication2/infrastructure/extentionsEthods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication2.Controllers;
+using System.Web.Mvc;
+using System.Text;
+
+namespace WebApplication2.infrastructure
+{
+    public static class extentionsEthods 
+    {
+        public static IQueryable<User> ByUserName(this IQueryable<User> query,string username)
+        {
+            return   query.Where(x => x.Name == username);
+        }
+       
+    }
+}
